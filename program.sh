53e8332f12bb0fa8b7462855e664365ac8624bdb
Gérardo <gerardoamouzouvi@gmail.com>
2025-08-07 00:19:55 +0200

    Update

--- program.sh
+++ program.sh
@@ -3,9 +3,12 @@
 user=$(whoami)
 date=$(date)
 dir_location=$(pwd)
+private_ip=$(hostname -I | awk '{print $1}')
+public_ip=$(wget -qO- https://api.ipify.org)
+
 
 echo "Welcome, $user"
 echo "Today is $date"
 echo "You're currently in the directory $dir_location"
-
-
+echo "Your private ip address is $private_ip"
+echo "Your public ip is : $public_ip"
